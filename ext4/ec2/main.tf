5e0593e001359c3a6e657fd08611fe3c630e5b4e
kthullib <kondalarao.thullibilli@oracle.com>
2023-02-28 22:12:41 +0530

    Modified

---
+++ ext4/ec2/main.tf
@@ -0,0 +1,26 @@
+data "aws_ami" "ami" {
+	most_recent = true
+	name_regex = "Centos-8-Devop-Practice"
+	owners = ["973714476881"]
+
+}
+
+resource "aws_instance" "ec2" {
+ami= data.aws_ami.ami.image_id
+instance_type = var.instance_type
+vpc_security_group_ids=["sg-05b87889bc82fcb3e"]
+tags = {
+Name=var.component
+}
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z014076334KCXLGIY0AWV"
+  name    = "${var.component}-dev.devopsb71.xyz"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
